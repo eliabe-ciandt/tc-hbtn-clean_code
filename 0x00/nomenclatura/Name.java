6e6cdb2360998037be87e4542d09ed754557cb48
Eliabe-ciandt <eliabe@ciandt.com>
2022-07-18 13:29:39 -0300

    fix 1

--- 0x00/nomenclatura/Name.java
+++ 0x00/nomenclatura/Name.java
@@ -4,11 +4,8 @@ import java.util.Date;
 public class Name {
     //variáveis que falam por si só
     //A
-//    public Date modifiedDate;
-//    public List<Testcase> findAllTestcasesByUser(Name.User user){};
-    //B
-    public Date d; // modified date
-    public List<Testcase> find(Name.User user){}; //find all Testcases by User
+    public Date modifiedDate;
+    public List<Testcase> findAllTestcasesByUser(Name.User user){};
 
     //Declarações corretas
     //A
@@ -17,36 +14,24 @@ public class Name {
 //    public Account[] AccountList;
 
     // Sem Prefixos
-    //A
-    public void deleteUser(Name.User pUser){};
 //    //B
-//    public void deleteUser(Name.User user){};
+    public void deleteUser(Name.User user){};
 
     // Evite mapa mental
-//    A
-    public URL r;
 //    //B
-//    public URL url;
+    public URL url;
 
     // Classe possui nomes próprios, funções devem ter verbos
     //A
     public class TableRepresent {};
     public void saveCar(){};
-//    //B
-//    public class TableRepresenter{};
-//    public void car(){};
 
     // Uma palavra por conceito
-//    //A
-//    public saveUser(){}; saveAccount(){}; saveContract(){};
     //B
     public saveUser(){}; createAccount(){}; generateContract(){};
 
 
     //Use constants, enums instead of magic numbers, abbreviations
-//    //A
-//    public int daysInYear = 360;
-//    public int hoursInWeek = 168;
     //B
     public final int DAYS_IN_YEAR = 360;
     public final int HOURS_IN_WEEK = 168;
